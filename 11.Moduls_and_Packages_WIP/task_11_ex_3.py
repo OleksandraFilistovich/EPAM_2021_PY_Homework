73e3a533f23596f53feb04bf71f2316d8a230b31
kolvDpaP <alexafiliho@gmail.com>
2021-12-14 13:52:36 +0200

    11_add_3

--- 11.Moduls_and_Packages_WIP/task_11_ex_3.py
+++ 11.Moduls_and_Packages_WIP/task_11_ex_3.py
@@ -1,6 +1,5 @@
 """
 Task 3
-
 Implement a decorator `call_once` which runs `sum_of_numbers` function once and caches the result.
 All consecutive calls to this function should return cached result no matter the arguments.
 
@@ -21,6 +20,18 @@ print(sum_of_numbers(134, 412))
 
 >>> 55
 """
+
+memory = None
+
+
+def call_once(fn):
+    def wrapper(*args):
+        global memory
+        if not memory:
+            memory = fn(*args)
+        return memory
+    return wrapper
+
 
 @call_once
 def sum_of_numbers(a, b):
